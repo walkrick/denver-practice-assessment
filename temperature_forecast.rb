83bcfc4f8a457359f9085a8c42c69d9ba81c29e5
Hunter Gillane <hunter.gillane@gmail.com>
2014-06-22 21:17:39 -0600

    add weather forecast problem

---
+++ temperature_forecast.rb
@@ -0,0 +1,51 @@
+forecast = [
+  {
+    "day" => "monday",
+    "temperatures" => {
+      "high" => 72,
+      "low" => 52
+    }
+  },
+  {
+    "day" => "tuesday",
+    "temperatures" => {
+      "high" => 73,
+      "low" => 57
+    }
+  },
+  {
+    "day" => "wednesday",
+    "temperatures" => {
+      "high" => 80,
+      "low" => 56
+    }
+  },
+  {
+    "day" => "thursday",
+    "temperatures" => {
+      "high" => 81,
+      "low" => 58
+    }
+  },
+  {
+    "day" => "friday",
+    "temperatures" => {
+      "high" => 81,
+      "low" => 55
+    }
+  },
+  {
+    "day" => "saturday",
+    "temperatures" => {
+      "high" => 82,
+      "low" => 57
+    }
+  },
+  {
+    "day" => "sunday",
+    "temperatures" => {
+      "high" => 88,
+      "low" => 60
+    }
+  }
+]
